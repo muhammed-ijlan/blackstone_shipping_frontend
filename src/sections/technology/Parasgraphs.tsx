7cbe7977e0af772a33b3816990840ae6d7e7b679
Muhammed Ijlan <ijlanijlu580@gmail.com>
2025-06-23 14:38:44 +0530

    updates

--- src/sections/technology/Parasgraphs.tsx
+++ src/sections/technology/Parasgraphs.tsx
@@ -1,14 +1,14 @@
-import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
-import { TechnologyData } from 'src/types/graphql/types/technology.types'
+// import { Box, Stack, Typography } from '@mui/material'
+// import React from 'react'
+// import { TechnologyData } from 'src/types/graphql/types/technology.types'
 
-const Parasgraphs = ({data}:{data:TechnologyData}) => {
-  return (
-    <Stack sx={{my:5}}>
-        <Typography variant='h1'sx={{textTransform:"capitalize !important"}}>{data.technology.title} : {data.technology.technologySinglePageOptions?.subTitle}</Typography>
-       <Box component={"div"} dangerouslySetInnerHTML={{__html:data.technology.content}}/>
-    </Stack>
-  )
-}
+// const Parasgraphs = ({data}:{data:TechnologyData}) => {
+//   return (
+//     <Stack sx={{my:5}}>
+//         {/* <Typography variant='h1'sx={{textTransform:"capitalize !important"}}>{data.technology.title} : {data.technology.technologySinglePageOptions?.subTitle}</Typography>
+//        <Box component={"div"} dangerouslySetInnerHTML={{__html:data.technology.content}}/> */}
+//     </Stack>
+//   )
+// }
 
-export default Parasgraphs
+// export default Parasgraphs
