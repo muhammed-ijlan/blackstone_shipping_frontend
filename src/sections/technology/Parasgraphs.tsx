1bcc0e72ffc7dd2205c0e99e266edf96014cd62b
Muhammed Ijlan <ijlanijlu580@gmail.com>
2025-07-04 18:32:58 +0530

    updates

--- src/sections/technology/Parasgraphs.tsx
+++ src/sections/technology/Parasgraphs.tsx
@@ -11,42 +11,42 @@ const Parasgraphs = ({ data }: { data: Data }) => {
   console.log( <Box
     component={"div"}
     sx={{
-      "& p": {
-        fontWeight:"600",
-        typography:"body1",
-        margin:"0"
-      },
-      "& strong": {
-        fontWeight:"600",
-        typography:"h4",
-      },
-      "& ul": {
-            padding: 0,
-            display: "flex",
-            flexWrap: "wrap",
-            gap: 2,
-            justifyContent: "space-between",
-            margin: 0,
-          },
-          "& li": {
-            typography: "body1",
-            flex: "1 1 48%",
-            fontSize: "1rem",
-            border: "1px solid rgba(109, 110, 113, 0.1)",
-            padding: "15px 15px",
-            borderRadius: "4px",
-            backgroundColor: "rgba(249, 250, 251, 1)",
-            position: "relative",
-            paddingLeft: "30px",
-          },
-          "& li::before": {
-            content: '"•"',
-            position: "absolute",
-            left: "12px",
-            top: "50%",
-            transform: "translateY(-50%)",
-            fontSize: "1.2rem",
-          },
+      // "& p": {
+      //   fontWeight:"600",
+      //   typography:"body1",
+      //   margin:"0"
+      // },
+      // "& strong": {
+      //   fontWeight:"600",
+      //   typography:"h4",
+      // },
+      // "& ul": {
+      //       padding: 0,
+      //       display: "flex",
+      //       flexWrap: "wrap",
+      //       gap: 2,
+      //       justifyContent: "space-between",
+      //       margin: 0,
+      //     },
+      //     "& li": { 
+      //       typography: "body1",
+      //       flex: "1 1 48%",
+      //       fontSize: "1rem",
+      //       border: "1px solid rgba(109, 110, 113, 0.1)",
+      //       padding: "15px 15px",
+      //       borderRadius: "4px",
+      //       backgroundColor: "rgba(249, 250, 251, 1)",
+      //       position: "relative",
+      //       paddingLeft: "30px",
+      //     },
+      //     "& li::before": {
+      //       content: '"•"',
+      //       position: "absolute",
+      //       left: "12px",
+      //       top: "50%",
+      //       transform: "translateY(-50%)",
+      //       fontSize: "1.2rem",
+      //     },
     }}
 
     dangerouslySetInnerHTML={{ __html: data.content }}
