36adb997f0f406b7f68d6d7cf8d87790334db09b
Muhammed Ijlan <ijlanijlu580@gmail.com>
2025-09-18 12:11:41 +0530

    updates

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://blackstone.hexprojects.in/admin/graphql",
+  uri: "https://wp.blackstoneshipping.com/graphql",
   cache: new InMemoryCache(),
 });
 
